85cc31bda130de70748e3d6f8bb916c4955f0e0f
libar <libar125@outlook.com>
2023-08-24 13:48:31 +0800

    Knife4j & smart-doc

--- back-api/src/main/java/com/qing/fapi/WebfapiApplication.java
+++ back-api/src/main/java/com/qing/fapi/WebfapiApplication.java
@@ -2,8 +2,10 @@ package com.qing.fapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication(scanBasePackages = "com.qing")
+@EnableSwagger2WebMvc
 public class WebfapiApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebfapiApplication.class);
